a61784081f51f18fa087afc68dfd35ed0bcdd25b
eslamfaisal <eslam.faisal.ef@gmail.com>
2022-01-07 00:23:55 +0200

    update android and client screen

--- lib/screens/clients/view/clients_details_screen.dart
+++ lib/screens/clients/view/clients_details_screen.dart
@@ -1,11 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:instalment/screens/base_view.dart';
 import 'package:instalment/screens/clients/model/clients_model.dart';
-import 'package:instalment/screens/clients/viewmodel/clients_view_model.dart';
 
 class ClientsDetailsScreen extends StatelessWidget {
-
   ClientModel clientModel;
   ClientsDetailsScreen(this.clientModel, {Key? key}) : super(key: key);
 
@@ -14,22 +11,60 @@ class ClientsDetailsScreen extends StatelessWidget {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: Column(
+        appBar: AppBar(
+          bottom: const TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.details)),
+              Tab(icon: Icon(Icons.monetization_on_outlined)),
+            ],
+          ),
+        ),
+        body: TabBarView(
           children: [
-            ListTile(
-              title: Text(tr('name')),
-              subtitle: Text(clientModel.name),
+            Column(
+              children: [
+                ListTile(
+                  title: Text(tr('name')),
+                  subtitle: Text(clientModel.name),
+                ),
+                ListTile(
+                  title: Text(tr('phone')),
+                  subtitle: Text(clientModel.phone),
+                ),
+                ListTile(
+                  title: Text(tr('address')),
+                  subtitle: Text(clientModel.address),
+                ),
+                ListTile(
+                  title: Text(tr('national_id')),
+                  subtitle: Text(clientModel.id_num),
+                ),
+              ],
             ),
-            ListTile(
-              title: Text(tr('phone')),
-              subtitle: Text(clientModel.phone),
+            Column(
+              children: [
+                ListTile(
+                  title: Text(tr('name')),
+                  subtitle: Text(clientModel.name),
+                ),
+                ListTile(
+                  title: Text(tr('phone')),
+                  subtitle: Text(clientModel.phone),
+                ),
+                ListTile(
+                  title: Text(tr('address')),
+                  subtitle: Text(clientModel.address),
+                ),
+                ListTile(
+                  title: Text(tr('national_id')),
+                  subtitle: Text(clientModel.id_num),
+                ),
+              ],
             ),
-            ListTile(
-              title: Text(tr('address')),
-              subtitle: Text(clientModel.address),
-            ),
+
           ],
         ),
+
         floatingActionButton: FloatingActionButton(
           onPressed: () {},
           child: const Icon(Icons.add),
